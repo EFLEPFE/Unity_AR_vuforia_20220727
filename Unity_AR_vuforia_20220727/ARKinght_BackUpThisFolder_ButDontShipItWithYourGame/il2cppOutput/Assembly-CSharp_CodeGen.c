a501e56a48c77e7cdd8690201ae9b7eba1a5515e
EFLEPFE <105775244+EFLEPFE@users.noreply.github.com>
2022-07-27 16:30:15 +0800

    Update 只剩apk壓縮

---
+++ Unity_AR_vuforia_20220727/ARKinght_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void neat.AR.Vuforia.CardManager::Awake()
+extern void CardManager_Awake_mDA8630BE7DD58ECEDBE03A5CA6B5E77B61F2BE54 (void);
+// 0x00000002 System.Void neat.AR.Vuforia.CardManager::OnJumpPressed(Vuforia.VirtualButtonBehaviour)
+extern void CardManager_OnJumpPressed_m9D9D20F557A9E986C7958A534CA80934DD4F4B7E (void);
+// 0x00000003 System.Void neat.AR.Vuforia.CardManager::CardFound()
+extern void CardManager_CardFound_m362B80843F4D6BD315F99A99EF1F57E1D912782F (void);
+// 0x00000004 System.Void neat.AR.Vuforia.CardManager::CardLost()
+extern void CardManager_CardLost_m8ADF9FF7C2E131A0BB2A1189CC720D053C105438 (void);
+// 0x00000005 System.Void neat.AR.Vuforia.CardManager::Attack()
+extern void CardManager_Attack_m40FAF91E23FF00EC873300142989D600F22AC0DB (void);
+// 0x00000006 System.Void neat.AR.Vuforia.CardManager::.ctor()
+extern void CardManager__ctor_mD68CA61BAF59F18F29040B790BA67C4106E964A6 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CardManager_Awake_mDA8630BE7DD58ECEDBE03A5CA6B5E77B61F2BE54,
+	CardManager_OnJumpPressed_m9D9D20F557A9E986C7958A534CA80934DD4F4B7E,
+	CardManager_CardFound_m362B80843F4D6BD315F99A99EF1F57E1D912782F,
+	CardManager_CardLost_m8ADF9FF7C2E131A0BB2A1189CC720D053C105438,
+	CardManager_Attack_m40FAF91E23FF00EC873300142989D600F22AC0DB,
+	CardManager__ctor_mD68CA61BAF59F18F29040B790BA67C4106E964A6,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	5045,
+	4003,
+	5045,
+	5045,
+	5045,
+	5045,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
--- Unity_AR_vuforia_20220727/Assets/Scrips/CardManager.cs
+++ Unity_AR_vuforia_20220727/Assets/Scrips/CardManager.cs
@@ -1,6 +1,9 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
 
-namespace neat
+namespace neat.AR.Vuforia
 {
     /// <summary>
     /// 圖片辨識管理器
@@ -9,14 +12,31 @@ namespace neat
     {
         [SerializeField,Header("KID 圖片")]
         private DefaultObserverEventHandler observerKID;
+        [SerializeField, Header("騎士")]
+        private Animator aniknight;
+        [SerializeField,Header("攻擊按鈕")]
+        private Button butAttack;
+        [SerializeField, Header("虛擬按鈕跳躍")]
+        private VirtualButtonBehaviour vbbJump;
 
+        private AudioSource audBGM;
+        private string parVictory = "觸發勝利";
+        
 
         private void Awake()
         {
             observerKID.OnTargetFound.AddListener(CardFound);
-            observerKID.OnTargetFound.AddListener(CardLost);
+            observerKID.OnTargetLost.AddListener(CardLost);
+            butAttack.onClick.AddListener(Attack);
+            vbbJump.RegisterOnButtonPressed(OnJumpPressed);
+            audBGM = GameObject.Find("BGM").GetComponent<AudioSource>();
         }
 
+        private void OnJumpPressed(VirtualButtonBehaviour obj)
+        {
+            print("<Color=#3366dd>跳躍</color>");
+            
+        }
 
         private void CardFound() 
         {
@@ -24,6 +44,9 @@ namespace neat
             /// 圖片辨識成功
             /// </summary>
             print("<color=yellow>找到卡片</color>");
+            aniknight.SetTrigger(parVictory);
+            audBGM.Play();
+            
         }
 
         private void CardLost()
@@ -32,6 +55,12 @@ namespace neat
             /// 圖片辨識失敗
             /// </summary>
             print("<color=red>卡片辨識失敗</color>");
+            audBGM.Stop();
+        }
+
+        private void Attack() 
+        {
+            print("<color=#9955aa>攻擊</color>");
         }
     }
 
